b120bf0a2bccb19566559b6dfbe6afd90300672e
ShravaniiiiS <shravanis.24.beai@acharya.ac.in>
2025-05-21 20:03:41 +0530

    Update CollatzSequence.py

--- CollatzSequence.py
+++ CollatzSequence.py
@@ -1,3 +1,11 @@
+st_name=input('enter your name')
+st_usn=input('enter the usn')
+st_sec=input('enter your section')
+print('\t\tname:'+st_name)
+print('\t\tusn:'+st_usn)
+print('\t\tsection:'+st_sec)
+print('=====================**********========================')
+
 n=int(input('enter a positive integer'))
 while n!=1:
     print(n,end='>')
