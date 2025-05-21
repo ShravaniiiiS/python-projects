00890ffba514099d5fd846e1ff7f4db9e79722a6
ShravaniiiiS <shravanis.24.beai@acharya.ac.in>
2025-05-21 19:53:18 +0530

    Create . TablePrinter.py

---
+++ . TablePrinter.py
@@ -0,0 +1,8 @@
+# Ask user for the number
+num = int(input("Enter a number to print its multiplication table: "))
+
+print(f"Multiplication table for {num}:")
+
+# Print table from 1 to 10
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
