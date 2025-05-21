bf5fb66274a3ad29557c0ea34cd4d8fdf1e3d9b9
ShravaniiiiS <shravanis.24.beai@acharya.ac.in>
2025-05-21 20:05:40 +0530

    Update Zigzag.py

--- Zigzag.py
+++ Zigzag.py
@@ -1,3 +1,11 @@
+st_name=input('enter your name')
+st_usn=input('enter the usn')
+st_sec=input('enter your section')
+print('\t\tname:'+st_name)
+print('\t\tusn:'+st_usn)
+print('\t\tsection:'+st_sec)
+print('=====================**********========================')
+
 def zigzag_pattern(rows, columns):
     for i in range(rows):
         for j in range(columns):
